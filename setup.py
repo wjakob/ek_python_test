5f114e92e32260e8834e191948586cf06098a0ac
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-11-12 22:58:19 +0100

    minor improvements

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from skbuild import setup
+import setuptools
+
+try:
+    from skbuild import setup
+    import pybind11
+except ImportError:
+    print("The preferred way to invoke 'setup.py' is via pip, as in 'pip "
+          "install .'. If you wish to run the setup script directly, you must "
+          "first install the build dependencies listed in pyproject.toml!",
+          file=sys.stderr)
+    raise
 
 setup(
     name="ek_python_test",
